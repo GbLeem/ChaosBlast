5492db8aeefe10c0cc3deadb2aed8fc662efe66c
GB_LEEM <juntrex@naver.com>
2024-01-16 12:54:54 +0900

    [ADD] project

---
+++ ChaosBlaster/Source/ChaosBlaster/ChaosBlaster.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ChaosBlaster.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ChaosBlaster, "ChaosBlaster" );
---
+++ ChaosBlaster/Source/ChaosBlaster/ChaosBlasterGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ChaosBlasterGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHAOSBLASTER_API AChaosBlasterGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
