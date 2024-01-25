0c1d18fe2d4ed448f01d42677b8c5b8d137525ef
GB_LEEM <juntrex@naver.com>
2024-01-25 23:49:27 +0900

    [ADD] variable replication widget

--- ChaosBlaster/Source/ChaosBlaster/Weapon/Weapon.cpp
+++ ChaosBlaster/Source/ChaosBlaster/Weapon/Weapon.cpp
@@ -1,5 +1,7 @@
 #include "Weapon.h"
 #include "Components/SphereComponent.h"
+#include "Components/WidgetComponent.h"
+#include "ChaosBlaster/Character/BlasterCharacter.h"
 
 AWeapon::AWeapon()
 {
@@ -7,7 +9,7 @@ AWeapon::AWeapon()
 	bReplicates = true;
 
 	WeaponMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("WeaponMesh"));
-	WeaponMesh->SetupAttachment(RootComponent);
+	//WeaponMesh->SetupAttachment(RootComponent);
 	SetRootComponent(WeaponMesh);
 
 	WeaponMesh->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Block);
@@ -19,17 +21,28 @@ AWeapon::AWeapon()
 	
 	AreaSphere->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
 	AreaSphere->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+	Pickupwidget = CreateDefaultSubobject<UWidgetComponent>(TEXT("PickupWidget"));
+	Pickupwidget->SetupAttachment(RootComponent);
+
 }
 
 void AWeapon::BeginPlay()
 {
 	Super::BeginPlay();
-	
-	if (GetLocalRole() == ENetRole::ROLE_Authority)
+	if (HasAuthority())
 	{
 		AreaSphere->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
 		AreaSphere->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Overlap);
+		AreaSphere->OnComponentBeginOverlap.AddDynamic(this, &ThisClass::OnSphereOverlap);
+		AreaSphere->OnComponentEndOverlap.AddDynamic(this, &ThisClass::OnSphereEndOverlap);
 	}
+
+	if (Pickupwidget)
+	{
+		Pickupwidget->SetVisibility(false);
+	}
+
 }
 
 void AWeapon::Tick(float DeltaTime)
@@ -37,4 +50,35 @@ void AWeapon::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 
 }
+
+void AWeapon::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	ABlasterCharacter* BlasterCharacter = Cast<ABlasterCharacter>(OtherActor);
+
+	if (BlasterCharacter)
+	{
+		//Pickupwidget->SetVisibility(true);
+		BlasterCharacter->SetOverlappingWeapon(this);
+	}
+}
+
+void AWeapon::OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex)
+{
+	ABlasterCharacter* BlasterCharacter = Cast<ABlasterCharacter>(OtherActor);
+
+	if (BlasterCharacter)
+	{		
+		BlasterCharacter->SetOverlappingWeapon(nullptr);
+	}
+}
+
+
+void AWeapon::ShowPickupWidget(bool bShowWidget)
+{
+	if (Pickupwidget)
+	{
+		Pickupwidget->SetVisibility(bShowWidget);
+	}
+}
+
 
